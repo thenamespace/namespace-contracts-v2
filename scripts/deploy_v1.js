c3a65c0a0b5bbd447e91719f8b4711e5c8515e97
nenadmitt <nenadmitt@gmail.com>
2023-12-28 20:46:20 +0100

    update-sepolia-contract-addesses

--- scripts/deploy_v1.js
+++ scripts/deploy_v1.js
@@ -1,9 +1,9 @@
 const hre = require("hardhat");
 
-const verifier = "0xBd0a4cF87ac861594E072d4779805C4EE954001b";
-const controller = "0x745b82391B5fE19235A86513ecaB61eB366536bD";
-const treasury = "0xEF604aD7c0C94d2DFeab8d0BCBf45eB355F8b531";
-const nameWrapper = "0x0635513f179D50A207757E05759CbD106d7dFcE8";
+const verifier = "";
+const controller = "";
+const treasury = "";
+const nameWrapper = "";
 
 async function deploy() {
   const deployer = await hre.ethers.deployContract("NamespaceDeployer", [
