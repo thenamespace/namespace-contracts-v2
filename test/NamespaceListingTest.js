3fa84ca80ad9e657757ae1482398839b564e8bb6
UrosZigic <uros.zigic@gmail.com>
2024-02-21 17:31:35 +0100

    tests to be universal

--- test/NamespaceListingTest.js
+++ test/NamespaceListingTest.js
@@ -5,29 +5,33 @@ const path = require("path");
 describe("NamespaceLister", () => {
     const ensName = process.env.ENS_NAME;
     const unmintedEnsName = process.env.UNMINTED_ENS_NAME;
+    const contractsOwnerAddress = process.env.CONTRACTS_OWNER_ADDRESS;
+    const nameWrapperProxyAddress = process.env.NAME_WRAPPER_PROXY_ADDRESS;
+    const namespaceRegistryAddress = process.env.NAMESPACE_REGISTRY_ADDRESS;
+    const nameWrappperAddress = process.env.NAME_WRAPPER_ADDRESS;
 
     before(async () => {
-        const contractsOwner = await ethers.getImpersonatedSigner("0x3E1e131E7e613D260F809E6BBE6Cbf7765EDC77f");
+        const contractsOwner = await ethers.getImpersonatedSigner(contractsOwnerAddress);
 
         impersonatedSigner = await ethers.getImpersonatedSigner(process.env.OWNER_OF_THE_NAME);
 
         [owner, addr2] = await ethers.getSigners();
         
         namespaceLister = await ethers.getContractFactory("NamespaceLister");
-        namespaceLister = await namespaceLister.deploy("0x0Ff41b99D7185B01bA47Ca85e9049166Cb3CD6bd", "0x0635513f179D50A207757E05759CbD106d7dFcE8", "0xEe0b1452efef8B68928beE72a2B54E9EA4EFc4Ab");
+        namespaceLister = await namespaceLister.deploy(nameWrapperProxyAddress, nameWrappperAddress, namespaceRegistryAddress);
 
         await namespaceLister.connect(owner).setController(impersonatedSigner.address, true);
         await owner.sendTransaction({ to: impersonatedSigner.address, value: ethers.parseEther("1") });
 
         const nameWrapperProxyArtifact = require(path.resolve(__dirname, "../artifacts/contracts/NameWrapperProxy.sol/NameWrapperProxy.json"));
         const nameWrapperProxyAbi = nameWrapperProxyArtifact.abi;
-        const nameWrapperProxy = new ethers.Contract("0x0ff41b99d7185b01ba47ca85e9049166cb3cd6bd", nameWrapperProxyAbi, contractsOwner);
+        const nameWrapperProxy = new ethers.Contract(nameWrapperProxyAddress, nameWrapperProxyAbi, contractsOwner);
 
         await nameWrapperProxy.connect(contractsOwner).setController(namespaceLister.target, true);
 
         const namespaceRegistryArtifact = require(path.resolve(__dirname, "../artifacts/contracts/NamespaceRegistry.sol/NamespaceRegistry.json"));
         const namespaceRegistryAbi = namespaceRegistryArtifact.abi;
-        const namespaceRegistry = new ethers.Contract("0xEe0b1452efef8B68928beE72a2B54E9EA4EFc4Ab", namespaceRegistryAbi, contractsOwner);
+        const namespaceRegistry = new ethers.Contract(namespaceRegistryAddress, namespaceRegistryAbi, contractsOwner);
 
         await namespaceRegistry.connect(contractsOwner).setController(namespaceLister.target, true);
     });
