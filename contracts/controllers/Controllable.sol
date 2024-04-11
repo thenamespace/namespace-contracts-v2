e1306f9478d0f070fc86569cc48cc67ef02d09c7
branzinho <branzinho9@gmail.com>
2024-04-10 21:51:09 -0700

    Removed unused import

--- contracts/controllers/Controllable.sol
+++ contracts/controllers/Controllable.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ~0.8.20;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/access/Ownable2Step.sol";
 
 contract Controllable is Ownable2Step {
